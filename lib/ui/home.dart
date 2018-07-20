437345bda32a2ff9e3caf3196f3810e222011d47
thiagopaiva99 <thiagopaiva99@gmail.com>
2018-07-19 22:48:47 -0300

    First Commit and Setting up the AppBar and the first TextField

---
+++ lib/ui/home.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new HomeState();
+  }
+
+
+}
+
+class HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Weight on Planet X"),
+        centerTitle: true,
+        backgroundColor: Colors.black38,
+      ),
+
+      backgroundColor: Colors.blueGrey,
+      body: new Container(
+        alignment: Alignment.topCenter,
+        child: new ListView(
+          padding: const EdgeInsets.all(2.5),
+          children: <Widget>[
+            // new Image.asset()
+            new Container(
+              margin: const EdgeInsets.all(3.0),
+              alignment: Alignment.center,
+              child: new Column(
+                children: <Widget>[
+                  new TextField(
+                    controller: null,
+                    keyboardType: TextInputType.number,
+                    decoration: new InputDecoration(
+                      labelText: 'Your Weight on Earth',
+                      hintText: 'In Pounds',
+                      icon: new Icon(
+                        Icons.person_outline
+                      )
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+}
